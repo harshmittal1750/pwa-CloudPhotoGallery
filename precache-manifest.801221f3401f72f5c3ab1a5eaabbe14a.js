8ded7d42b1088c3ef57bc293f1fab0d47c6aaebd
HARSH MITTAL <harshmittal1750@gmail.com>
2022-05-15 20:04:54 +0530

    Updates

---
+++ precache-manifest.801221f3401f72f5c3ab1a5eaabbe14a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119f76d73b45e97369e8c4b9289996af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e9b9d1d9be30fc05969",
+    "url": "/static/css/main.59511a44.chunk.css"
+  },
+  {
+    "revision": "2bede5ed07c6dabeb262",
+    "url": "/static/js/2.ab530c7c.chunk.js"
+  },
+  {
+    "revision": "69c1fcfd31099edc9fe347d34e8780e7",
+    "url": "/static/js/2.ab530c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9b9d1d9be30fc05969",
+    "url": "/static/js/main.facb936a.chunk.js"
+  },
+  {
+    "revision": "dda61c82274d2b1ba171",
+    "url": "/static/js/runtime-main.f3bdab4c.js"
+  }
+]);
